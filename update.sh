ec488f37ead39f930eecfb92084eb4400d0b7958
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2023-12-19 23:21:15 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,10 +3,6 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-if [$(test -d /tmp/test) == 1]; then
-   printf "\033[0;31m! Please follow the update commands at https://github.com/beans-are-gross/mcloud\033[0m \n" 1>&2
-   exit 1
-fi
 clear
 
 printf "\033[0;32m> Moving html folder to /var/www.\033[0m \n"
