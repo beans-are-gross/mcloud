d90464b8efb4805f96a406395f036f3b1bea0098
beans-are-gross <pmehl9091@gmail.com>
2023-12-19 20:27:12 -0600

    removed php error reporting

--- html/file.php
+++ html/file.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 require "./conn.php";
 $sql = "SELECT externalDir, type FROM files WHERE accountCookie=? AND id=?;";
 $stmt = mysqli_stmt_init($conn);
