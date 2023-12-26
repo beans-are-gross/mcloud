dbae9bada4de5d884085a4262a2dad1390c9c71e
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2023-12-25 19:34:09 -0600

    fixed file not found error

--- html/file.php
+++ html/file.php
@@ -11,7 +11,7 @@ mysqli_stmt_close($stmt);
 $sql = "SELECT name, externalDir, type FROM files WHERE userId=? AND id=?;";
 $stmt = mysqli_stmt_init($conn);
 mysqli_stmt_prepare($stmt, $sql);
-mysqli_stmt_bind_param($stmt, "ss", $_COOKIE['pwd'], $_GET['id']);
+mysqli_stmt_bind_param($stmt, "ss", $userId, $_GET['id']);
 mysqli_stmt_execute($stmt);
 mysqli_stmt_bind_result($stmt, $name, $externalDir, $type);
 mysqli_stmt_fetch($stmt);
