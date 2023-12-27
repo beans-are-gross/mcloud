ed3af6bec8b1d3011aa3b1ad1c5eca501c07b0a3
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2023-12-26 20:45:09 -0600

    Added a file count alert and moved the clearing folder warning

--- install.sh
+++ install.sh
@@ -10,18 +10,22 @@ sudo apt update && sudo apt install php libapache2-mod-php php-mysql apache2 mys
 
 clear
 
-printf "\033[0;33m! Your /var/www/html folder will be deleted, enter Y to continue.\033[0m \n"
-printf "> "
-read confirmClearFolder
+fileCount=$(find "/var/www/html" -maxdepth 1 -type f | wc -l)
 
-confirmClearFolder=$(echo "$confirmClearFolder" | tr '[:upper:]' '[:lower:]')
-
-printf "\n";
-
-if [ "$confirmClearFolder" != "y" ]; then
-   clear
-   printf "\033[0;31m! Script canceled\033[0m \n" 1>&2
-   exit 1
+if [ "$fileCount" -gt 1 ]; then
+   printf "\033[0;33m! Your /var/www/html/ folder will be cleared, enter Y to continue.\033[0m \n"
+   printf "> "
+   read confirmClearFolder
+   
+   confirmClearFolder=$(echo "$confirmClearFolder" | tr '[:upper:]' '[:lower:]')
+   
+   printf "\n";
+   
+   if [ "$confirmClearFolder" != "y" ]; then
+      clear
+      printf "\033[0;31m! Script canceled\033[0m \n" 1>&2
+      exit 1
+   fi
 fi
 
 printf "\033[0;32m> Deleting html folder and moving html folder to /var/www.\033[0m \n"
